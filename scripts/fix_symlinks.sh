f9ba2cf683ad33b18cd8d1ac55bbeaed38afe204
Sébastien Moretti <smoretti@unil.ch>
2025-07-08 15:04:59 +0200

    Fix for the ftp symlink which may not have an existing destination at build time

--- scripts/fix_symlinks.sh
+++ scripts/fix_symlinks.sh
@@ -4,8 +4,11 @@ cd build/client/
 for lnk in `find ../../public/ -type l | xargs ls -l | sed -e 's/^.*-> //'`; do
     name=`basename $lnk`;
     rm -rf $name;
-    ln -s $lnk $name;
+    ln -fs $lnk $name;
 done
+
+# Add the ftp symlink which may have issues at build time if the destination does not exist!
+ln -fs /mnt/FTPBGEE/ftp ftp
 
 exit
 
