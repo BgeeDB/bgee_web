c4515b6593286a2a2ae8e6a16514259a8d2cdcb1
Harald Detering <harald.detering@unil.ch>
2025-11-09 15:43:46 +0100

    Remove bottom x label in single-gene expr graph.

--- src/components/Heatmap/Renderer.jsx
+++ src/components/Heatmap/Renderer.jsx
@@ -379,7 +379,7 @@ export const Renderer = forwardRef(
       const x = xScale(name);
       const xCoord = x + xScale.bandwidth() / 2;
       // TODO: fix bottom label position (too low)
-      const yCoord = height - colorLegendHeight - MARGIN.top - MARGIN.bottom + 10;
+      const yCoord = height - colorLegendHeight;
       // const yCoord = height - colorLegendHeight;
       // const yCoord = boundsHeight + 10 + (i % 2) * 20; // stagger labels
 
@@ -453,10 +453,7 @@ export const Renderer = forwardRef(
         <g width={boundsWidth} height={boundsHeight} transform={`translate(${[marginLeft, MARGIN.top].join(',')})`}>
           <g>
             <g>{allShapes}</g>
-            <g>
-              {xLabelsTop}
-              {xLabelsBottom}
-            </g>
+            <g>{xLabelsTop}</g>
             <g transform={`translate(-${marginLeft - 10}, 5)`}>
               <Tree data={drilldown} yScale={yScale} toggleCollapse={onToggleExpandCollapse} labelFont="Open Sans" />
             </g>
